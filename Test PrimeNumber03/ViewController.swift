ff6afd7d36bceb44b8879ac75c26da6aefc66023
D7703_22 <d7703_22@promise.local>
2019-04-09 14:26:37 +0900

    Initial Commit

---
+++ Test PrimeNumber03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test PrimeNumber03
+//
+//  Created by D7703_22 on 2019. 4. 9..
+//  Copyright © 2019년 fb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
